3165f8a1136654b8581ac6ff1df604b8b1d7d4aa
shahanas <shahanasamf@gmail.com>
2026-03-10 14:10:59 +0530

    Add project files.

---
+++ LinkedlistPractice.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_1
+{
+   public  class LinkedlistPractice
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        public Node head = null;
+        public Node tail = null;
+
+        public void addNode(int data)
+        {
+            Node newnode = new Node(data);
+          if(head==null)
+            {
+                head = newnode;
+                
+            }
+            else
+            {
+                tail.next = newnode;
+            }
+            tail = newnode;
+        }
+        public void disply()
+        {
+            Node temp = head;
+            
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        public void insertAtBegning(int data)
+        {
+            Node newnode = new Node(data);
+            if(head==null)
+            {
+                head = newnode;
+                tail = newnode;
+            }
+            else
+            {
+                newnode.next = head;
+                head = newnode;
+            }
+        }
+        public void insertAtEnd(int data)
+        {
+            Node newnode = new Node(data);
+            tail.next = newnode;
+            tail = newnode;
+        }
+
+        public void deletenode(int data)
+        {
+            Node temp = head, prev = null;
+
+            if(temp!=null && temp.data==data)
+            {
+                head = temp.next;
+                return;
+            }
+            while(temp!=null && temp.data!=data)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+            if(temp==null)
+            {
+                return;
+            }
+            prev.next = temp.next;
+        }
+
+        public void Insertatspecific(int data, int position)
+        {
+            Node newnode = new Node(data);
+            if(position==1)
+            {
+                newnode.next = head;
+                head = newnode;
+                if(tail==null)
+                {
+                    tail = newnode;
+                }
+                return;
+            }
+            Node temp = head;
+            for(int i=1;i<position-1;i++)
+            {
+                if (temp == null)
+                {
+                    Console.WriteLine("Position out of range");
+                    return;
+                }
+                temp = temp.next;
+            }
+            newnode.next = temp.next;
+            temp.next = newnode;
+
+            if(newnode.next==null)
+            {
+                tail = newnode;
+            }
+        }
+        public void kthelement(int k)
+        {
+            int count = 0;
+            Node temp = head;
+            if(temp!=null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            int position = count - k + 1;
+
+            temp = head;
+            for(int i=1;i<position;i++)
+            {
+                temp = temp.next;
+            }
+            Console.WriteLine(temp.data);
+        }
+
+        public void findmiddle()
+        {
+            Node slow = head;
+            Node fast = head;
+            while(fast!=null && fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            Console.WriteLine(slow.data);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using DataStructure_1;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+
+            Program obj = new Program();
+            //obj.arrayReverse();
+            //obj.secondLargest();
+            //obj.Kthlargets();
+
+
+
+            //singly linkedlist
+
+            LinkedlistPractice practice = new LinkedlistPractice();
+            practice.addNode(12);
+            practice.addNode(15);
+            
+            practice.insertAtBegning(20);
+            practice.insertAtEnd(30);
+            practice.Insertatspecific(32, 3);
+            practice.disply();
+            practice.kthelement(3);
+            practice.findmiddle();
+        }
+
+
+
+
+        public void arrayReverse()
+        {
+            int[] arr = { 2, 3, 4, 5, 6 };
+            int start = 0;
+            int end = arr.Length - 1;
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+        //second largest number in an array
+        public void secondLargest()
+        {
+            int[] arr = { 2, 3, 4, 5, 6 };
+
+            int largest = 0;
+            int secondlarge = 0;
+            int thirdlarge = 0;
+
+            foreach(var num in arr)
+            {
+                if (num > largest)
+                {
+                    thirdlarge = secondlarge;
+                    secondlarge = largest;
+                    largest = num;
+
+                }
+                else if (num > secondlarge && num != largest)
+                {
+                    thirdlarge = secondlarge;
+                    secondlarge = num;
+                }
+                else if (num > thirdlarge && num != secondlarge&& num!=largest)
+                {
+                    thirdlarge = num;
+                }
+            }
+            Console.WriteLine(thirdlarge);
+        }
+
+
+        public void Kthlargets()
+        {
+            int[] arr = { 5, 6, 7, 8, 3, 4 };
+            //3,4,5,6,7,8-sorted
+
+            int k = 3;
+            Array.Sort(arr);
+
+            int result = arr[arr.Length - k];
+            Console.WriteLine(result);
+        }
+    }
+
+}
